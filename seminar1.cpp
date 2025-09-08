35f1b105db24c6105291f893cc9e04fc5b3f6cf5
AnastasiaLeon <anastasialeo00@inbox.ru>
2025-09-08 19:05:45 +0300

    First seminar

--- seminar1.cpp
+++ seminar1.cpp
@@ -3,5 +3,6 @@
 int main(int argc, char** argv){
     std::cout << "First seminar" << std::endl;
     std::cout << "Second commit" << std::endl;
+    std::cout << "Third commit" << std::endl;
     return 0;
 }
